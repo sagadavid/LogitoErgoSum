9e00e1147744380b8d92c892ed455f1e9513583f
David Saga <epostsaga@gmail.com>
2024-05-25 16:51:14 +0200

    serilog write to file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,17 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 //builder.Logging.AddEventLog();//only for windows
+
+//third parthy logger to log to file
+try
+{
+    var seriLogger = new LoggerConfiguration().WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/serilog2file.txt"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 90).CreateLogger();
+    builder.Logging.AddSerilog(seriLogger);
+}
+catch (Exception ex)
+{
+    throw new Exception("seriLogger cant do the job", ex);
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
