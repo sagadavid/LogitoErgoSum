d8907895d308840b9eab59cbc4d969e4f082f11d
David Saga <epostsaga@gmail.com>
2024-05-25 17:24:41 +0200

    serilog2File extension

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using LogitoErgoSum;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,17 +12,22 @@ builder.Logging.AddDebug();
 //builder.Logging.AddEventLog();//only for windows
 
 //third parthy logger to log to file
-try
-{
-    var seriLogger = new LoggerConfiguration().WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/serilog2file.txt"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 90).CreateLogger();
-    builder.Logging.AddSerilog(seriLogger);
-}
-catch (Exception ex)
-{
-    throw new Exception("seriLogger cant do the job", ex);
-}
+// try
+// {
+//     var seriLogger = new LoggerConfiguration().WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/serilog2file.txt"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 90).CreateLogger();
+//     builder.Logging.AddSerilog(seriLogger);
+// }
+// catch (Exception ex)
+// {
+//     throw new Exception("seriLogger cant do the job", ex);
+// }
+
+//use extension instead
+builder.Logging.SeriLog2File();
+
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ SerilogService.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+namespace LogitoErgoSum;
+
+
+public static class SeriLogExtension
+{
+  public static ILoggingBuilder SeriLog2File(this ILoggingBuilder builder, string logFileName = "seriLog2File.txt", int retainedFileCountLimit = 90)
+  {
+    var logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", logFileName);
+
+    var logger = new LoggerConfiguration()
+        .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: retainedFileCountLimit)
+        .CreateLogger();
+
+    builder.AddSerilog(logger);
+
+    return builder;
+  }
+}
